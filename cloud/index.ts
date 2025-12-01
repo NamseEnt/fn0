b6f6a10e06e202fedf355e4fe13df0b9b7f6c7a3
namse <skatpgusskat@naver.com>
2025-12-01 17:42:36 +0900

    WIP: fn0 cloud iaac init

--- cloud/index.ts
+++ cloud/index.ts
@@ -1,1 +1,13 @@
 import * as pulumi from "@pulumi/pulumi";
+import * as cloudflare from "@pulumi/cloudflare";
+import { envCloudflareAccountId, envDomain } from "./env";
+
+const accountId = envCloudflareAccountId();
+const domain = envDomain();
+
+const zone = new cloudflare.Zone("zone", {
+  account: { id: accountId },
+  name: domain,
+  paused: false,
+  type: "full",
+});
