89c69392856abae5370b27b15960e48b056b78bf
namse <skatpgusskat@naver.com>
2026-01-19 17:07:04 +0900

    Add put/query/query_next methods to forte_doc macro and migrate docs
    - Add put() method to Doc struct for saving documents
    - Add query_next() method to Doc struct for pagination
    - Add DocQuery struct with send() method for querying documents
    - Update pk format to DocName/pk_field=value style
    - Flatten DeletedPostDoc and DeletedCommentDoc (remove nested structs)
    - Migrate all Doc usages to new forte_doc API

--- ls-news/rs/src/docs.rs
+++ ls-news/rs/src/docs.rs
@@ -1,53 +1,19 @@
+use forte_macros::forte_doc;
 use forte_sdk::{anyhow::Result, *};
-use serde::{Deserialize, Serialize};
 
-pub trait AsOptStr {
-    fn as_opt_str(&self) -> Option<&str>;
-}
-
-impl<T: AsRef<str>> AsOptStr for Option<T> {
-    fn as_opt_str(&self) -> Option<&str> {
-        self.as_ref().map(|s| s.as_ref())
-    }
-}
-
-impl<T: AsRef<str>> AsOptStr for &Option<T> {
-    fn as_opt_str(&self) -> Option<&str> {
-        self.as_ref().map(|s| s.as_ref())
-    }
-}
-
-impl AsOptStr for () {
-    fn as_opt_str(&self) -> Option<&str> {
-        None
-    }
-}
-
-#[derive(Serialize, Deserialize, Clone)]
+#[forte_doc]
 pub struct UserDoc {
+    #[sk]
     pub id: String,
     pub username: String,
     pub avatar_url: String,
     pub created_at: DateTime,
     pub updated_at: DateTime,
 }
-impl UserDoc {
-    pub async fn get(sk: impl AsRef<str>) -> Result<Option<Self>> {
-        Ok(forte_db::turso()
-            .get("users", sk.as_ref())
-            .await?
-            .map(|data| serde_json::from_slice(&data))
-            .transpose()?)
-    }
-    pub async fn put(sk: impl AsRef<str>, value: &Self) -> Result<()> {
-        forte_db::turso()
-            .put("users", sk.as_ref(), &serde_json::to_vec(value)?)
-            .await
-    }
-}
 
-#[derive(Serialize, Deserialize, Clone)]
-pub struct Post {
+#[forte_doc]
+pub struct PostDoc {
+    #[sk]
     pub id: String,
     pub title: String,
     pub url: String,
@@ -58,45 +24,14 @@ pub struct Post {
     pub created_at: DateTime,
     pub updated_at: DateTime,
 }
-impl Post {
-    pub async fn get(sk: impl AsRef<str>) -> Result<Option<Self>> {
-        Ok(forte_db::turso()
-            .get("posts", sk.as_ref())
-            .await?
-            .map(|data| serde_json::from_slice(&data))
-            .transpose()?)
-    }
-    pub async fn query(after_sk: impl AsOptStr, limit: usize) -> Result<Vec<Self>> {
-        Ok(forte_db::turso()
-            .query("posts", after_sk.as_opt_str(), limit)
-            .await?
-            .into_iter()
-            .map(|(_sk, data)| serde_json::from_slice(&data))
-            .collect::<Result<Vec<_>, _>>()?)
-    }
-}
 
-#[derive(Serialize, Deserialize, Clone)]
-pub struct DeletedPost {
-    pub post: Post,
-    pub deleted_at: DateTime,
-}
-impl DeletedPost {
-    pub async fn query(after_sk: impl AsOptStr, limit: usize) -> Result<Vec<Self>> {
-        Ok(forte_db::turso()
-            .query("deleted_posts", after_sk.as_opt_str(), limit)
-            .await?
-            .into_iter()
-            .map(|(_sk, data)| serde_json::from_slice(&data))
-            .collect::<Result<Vec<_>, _>>()?)
-    }
-}
-
-#[derive(Serialize, Deserialize, Clone)]
-pub struct Comment {
+#[forte_doc]
+pub struct CommentDoc {
+    #[pk]
+    pub post_id: String,
+    #[sk]
     pub id: String,
     pub content: String,
-    pub post_id: String,
     pub author_id: String,
     pub parent_comment_id: Option<String>,
     pub likes: usize,
@@ -104,46 +39,34 @@ pub struct Comment {
     pub created_at: DateTime,
     pub updated_at: DateTime,
 }
-impl Comment {
-    pub async fn query(
-        pk: impl AsRef<str>,
-        after_sk: impl AsOptStr,
-        limit: usize,
-    ) -> Result<Vec<Self>> {
-        Ok(forte_db::turso()
-            .query(
-                format!("comments#post_id={}", pk.as_ref()),
-                after_sk.as_opt_str(),
-                limit,
-            )
-            .await?
-            .into_iter()
-            .map(|(_sk, data)| serde_json::from_slice(&data))
-            .collect::<Result<Vec<_>, _>>()?)
-    }
-}
 
-#[derive(Serialize, Deserialize, Clone)]
-pub struct DeletedComment {
-    pub comment: Comment,
+#[forte_doc]
+pub struct DeletedPostDoc {
+    #[sk]
+    pub id: String,
+    pub title: String,
+    pub url: String,
+    pub content: String,
+    pub author_id: String,
+    pub likes: usize,
+    pub dislikes: usize,
+    pub created_at: DateTime,
+    pub updated_at: DateTime,
     pub deleted_at: DateTime,
 }
 
-impl DeletedComment {
-    pub async fn query(
-        pk: impl AsRef<str>,
-        after_sk: impl AsOptStr,
-        limit: usize,
-    ) -> Result<Vec<Self>> {
-        Ok(forte_db::turso()
-            .query(
-                format!("deleted_comments#post_id={}", pk.as_ref()),
-                after_sk.as_opt_str(),
-                limit,
-            )
-            .await?
-            .into_iter()
-            .map(|(_sk, data)| serde_json::from_slice(&data))
-            .collect::<Result<Vec<_>, _>>()?)
-    }
+#[forte_doc]
+pub struct DeletedCommentDoc {
+    #[pk]
+    pub post_id: String,
+    #[sk]
+    pub id: String,
+    pub content: String,
+    pub author_id: String,
+    pub parent_comment_id: Option<String>,
+    pub likes: usize,
+    pub dislikes: usize,
+    pub created_at: DateTime,
+    pub updated_at: DateTime,
+    pub deleted_at: DateTime,
 }
