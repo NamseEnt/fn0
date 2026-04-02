d8e40c7464ea3b0753833f23993ceae3de65c30b
namse <skatpgusskat@naver.com>
2026-04-02 12:24:37 +0900

    Allow oci-rust-sdk in Docker build context, exclude only target/

--- infra/pulumi/OciHeadQuarter/docker-registry.ts
+++ infra/pulumi/OciHeadQuarter/docker-registry.ts
@@ -82,8 +82,8 @@ export function createDockerRegistry(
     {
       imageName: pulumi.interpolate`${registryUrl}/${repo.namespace}/${repo.displayName}:v1`,
       build: {
-        context: "../../..",
-        dockerfile: "../../../hq/Dockerfile",
+        context: "../..",
+        dockerfile: "../../hq/Dockerfile",
         platform: "linux/arm64",
       },
       registry: {
