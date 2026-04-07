24db0dbaf08afe0fe497df0226eb3093e4068e7a
namse <skatpgusskat@naver.com>
2026-04-07 09:34:20 +0900

    Add DRG for IPv6 VCN peering between HQ and Worker

--- infra/pulumi/OciHeadQuarter/networking.ts
+++ infra/pulumi/OciHeadQuarter/networking.ts
@@ -9,10 +9,14 @@ export function createNetworking(
     compartmentId,
     vcnId,
     ipv6cidrBlocks,
+    drgId,
+    workerIpv6CidrBlocks,
   }: {
     compartmentId: pulumi.Input<string>;
     vcnId: pulumi.Input<string>;
     ipv6cidrBlocks: pulumi.Input<string[]>;
+    drgId: pulumi.Input<string>;
+    workerIpv6CidrBlocks: pulumi.Input<string[]>;
   }
 ): {
   regionalSubnet: oci.core.Subnet;
@@ -31,18 +35,25 @@ export function createNetworking(
     {
       compartmentId,
       vcnId,
-      routeRules: [
-        {
-          destination: "::/0",
-          destinationType: "CIDR_BLOCK",
-          networkEntityId: internetGateway.id,
-        },
-        {
-          destination: "0.0.0.0/0",
-          destinationType: "CIDR_BLOCK",
-          networkEntityId: internetGateway.id,
-        },
-      ],
+      routeRules: pulumi
+        .all([workerIpv6CidrBlocks])
+        .apply(([workerCidrs]) => [
+          {
+            destination: "::/0",
+            destinationType: "CIDR_BLOCK",
+            networkEntityId: internetGateway.id,
+          },
+          {
+            destination: "0.0.0.0/0",
+            destinationType: "CIDR_BLOCK",
+            networkEntityId: internetGateway.id,
+          },
+          ...workerCidrs.map((cidr: string) => ({
+            destination: cidr,
+            destinationType: "CIDR_BLOCK",
+            networkEntityId: drgId as string,
+          })),
+        ]),
     },
     { parent }
   );
